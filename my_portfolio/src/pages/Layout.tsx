f948f60d1ba8ee18815916f2ca0fd4b13fb32bab
Wang55566 <wang6@hawaii.edu>
2024-02-15 16:19:02 -0800

    commit

--- my_portfolio/src/pages/Layout.tsx
+++ my_portfolio/src/pages/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import {Analytics} from '@vercel/analytics/react'
 
 const Layout = () => {
   return (
     <>
       <NavBar />
       <Outlet />
+      <Analytics />
     </>
   );
 };
